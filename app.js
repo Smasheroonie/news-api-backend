7572145a95416253fd02797326630fd35d4b17f2
Luke_G_WSL <68129402+Smasheroonie@users.noreply.github.com>
2024-11-25 14:12:21 +0000

    complete GET /api/topics

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 const endpointsJson = require("./endpoints.json");
-
-app.use(express.json());
+const { handleServerError } = require("./controllers/errors-controller");
+const { getTopics } = require("./controllers/topics-controller");
 
 app.get("/api", (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
 });
+
+app.get("/api/topics", getTopics);
+
+app.use(handleServerError);
 
 module.exports = app;
